b8941eb5293d7316f663a1473241aab12d9b7c1a
Jack-Culcasi <giacomofculcasi@gmail.com>
2023-12-16 19:34:24 +0000

    Role Added in User Class

--- app/models.py
+++ app/models.py
@@ -14,7 +14,8 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
-    garage = db.relationship('Car', backref='owner', lazy='dynamic') 
+    garage = db.relationship('Car', backref='owner', lazy='dynamic')
+    role = db.Column(db.String(20), default='user')  
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
