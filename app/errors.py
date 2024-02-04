bc7cba428d3c0aaf150daac219afaf69996fae92
Jack-Culcasi <giacomofculcasi@gmail.com>
2024-02-04 21:26:08 +0000

    New Version

--- app/errors.py
+++ app/errors.py
@@ -1,9 +1,10 @@
 from flask import render_template
 from app import app, db
+from flask_login import current_user, login_user, logout_user, login_required
 
 @app.errorhandler(404)
 def not_found_error(error):
-    return render_template('404.html'), 404
+    return render_template('404.html', user_name=current_user.username), 404
 
 @app.errorhandler(500)
 def internal_error(error):
