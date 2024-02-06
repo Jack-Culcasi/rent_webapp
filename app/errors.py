5afb4f925958e765ab194903a7ffc6083ae6a856
Jack-Culcasi <giacomofculcasi@gmail.com>
2024-02-06 14:47:51 +0000

    User_name bug fixed

--- app/errors.py
+++ app/errors.py
@@ -4,9 +4,17 @@ from flask_login import current_user, login_user, logout_user, login_required
 
 @app.errorhandler(404)
 def not_found_error(error):
-    return render_template('404.html', user_name=current_user.username), 404
+    if current_user.is_authenticated:
+        user_name = current_user.username
+    else:
+        user_name = None
+    return render_template('404.html', user_name=user_name), 404
 
 @app.errorhandler(500)
 def internal_error(error):
+    if current_user.is_authenticated:
+        user_name = current_user.username
+    else:
+        user_name = None
     db.session.rollback()
-    return render_template('500.html', user_name=current_user.username), 500
+    return render_template('500.html', user_name=user_name), 500
