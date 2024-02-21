9c0552a1c012f5c74ca3158ab757c50427a5a971
Jack-Culcasi <giacomofculcasi@gmail.com>
2024-02-21 17:10:56 +0000

    Sorting algorithm fixed

--- app/static/js/sorting.js
+++ app/static/js/sorting.js
@@ -1,21 +1,32 @@
 $(document).ready(function() {
-    $('.sort-link').click(function(e) {
+  $('.sort-link').click(function(e) {
       e.preventDefault();
       const sortBy = $(this).data('sort');
-      sortTable(sortBy);
-    });
-  
-    function sortTable(sortBy) {
-      const rows = $('#carTable tbody tr').get();
-  
-      rows.sort(function(rowA, rowB) {
-        const valueA = $(rowA).find(`td[data-sort="${sortBy}"]`).text().toUpperCase();
-        const valueB = $(rowB).find(`td[data-sort="${sortBy}"]`).text().toUpperCase();
-  
-        return valueA.localeCompare(valueB);
-      });
-  
-      $('#carTable tbody').empty().append(rows);
-    }
+      const tableId = $(this).closest('table').attr('id');
+      const sortOrder = $(this).data('sort-order') || 'asc'; // Default to ascending order if not previously set
+      const newSortOrder = sortOrder === 'asc' ? 'desc' : 'asc'; // Toggle sorting order
+
+      sortTable(tableId, sortBy, newSortOrder);
+      
+      // Update data-sort-order attribute to reflect the new sorting order
+      $(this).data('sort-order', newSortOrder);
   });
-  
+
+  function sortTable(tableId, sortBy, sortOrder) {
+      const rows = $(`#${tableId} tbody tr`).get();
+
+      rows.sort(function(rowA, rowB) {
+          let valueA = $(rowA).find(`td[data-sort="${sortBy}"]`).text().toUpperCase();
+          let valueB = $(rowB).find(`td[data-sort="${sortBy}"]`).text().toUpperCase();
+
+          // Toggle sorting order based on sortOrder
+          if (sortOrder === 'desc') {
+              [valueA, valueB] = [valueB, valueA]; // Swap values for descending order
+          }
+
+          return valueA.localeCompare(valueB);
+      });
+
+      $(`#${tableId} tbody`).empty().append(rows);
+  }
+});
