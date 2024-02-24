6cf4206068231a68658567c30567e55c2813b0b2
Jack-Culcasi <giacomofculcasi@gmail.com>
2024-02-24 20:37:47 +0000

    email logic added

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_mail import Mail, Message
 import logging
 from logging.handlers import SMTPHandler
 
@@ -13,6 +14,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+mail = Mail(app)
 
 if not app.debug:
     if app.config['MAIL_SERVER']:
@@ -22,12 +24,14 @@ if not app.debug:
         secure = None
         if app.config['MAIL_USE_TLS']:
             secure = ()
+        fromaddr = app.config['MAIL_USERNAME']  # Use the sender email address
         mail_handler = SMTPHandler(
             mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
-            fromaddr='no-reply@' + app.config['MAIL_SERVER'],
-            toaddrs=app.config['ADMINS'], subject='Microblog Failure',
+            fromaddr=fromaddr,  # Set the sender email address
+            toaddrs=app.config['ADMINS'], subject='Rentami Failure',
             credentials=auth, secure=secure)
         mail_handler.setLevel(logging.ERROR)
         app.logger.addHandler(mail_handler)
+
 
 from app import routes, models, errors
