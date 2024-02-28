1f71a200f7af90c5619294021db191b4c033ea55
Jack-Culcasi <giacomofculcasi@gmail.com>
2024-02-28 21:34:59 +0000

    shell context updated

--- rent_webapp.py
+++ rent_webapp.py
@@ -1,9 +1,10 @@
 from flask import request
 from app import app, db
-from app.models import User, Car, Booking
+from app.models import User, Car, Booking, Renewal, Contacts, Groups
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'Car': Car, 'Booking': Booking}
+    return {'db': db, 'User': User, 'Car': Car, 'Booking': Booking, 
+            'Renewal': Renewal, 'Contacts': Contacts, 'Groups': Groups}
 
 # export FLASK_DEBUG=1
