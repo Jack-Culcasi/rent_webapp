8fd816b9989cdbd5f671931f3129c4a45d5d2a6c
Jack-Culcasi <giacomofculcasi@gmail.com>
2024-03-02 20:22:50 +0000

    environmental variables set

--- config.py
+++ config.py
@@ -1,15 +1,14 @@
 import os
-import secrets
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or secrets.token_hex(16)
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    MAIL_SERVER = 'smtp-mail.outlook.com'
-    MAIL_PORT = 587
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = os.environ.get('MAIL_PORT')
     MAIL_USE_TLS = True
-    MAIL_USERNAME = 'rentami_team@outlook.com'
-    MAIL_PASSWORD = 'Missile_Protonico'
-    ADMINS = ['rentami_team@outlook.com']
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = os.environ.get('ADMINS')
